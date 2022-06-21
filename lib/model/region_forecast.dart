55a42296cbc2024e4be25d2fe660f1639d92af07
TaeXtream <59652919+taextreme@users.noreply.github.com>
2022-06-21 13:06:11 +0700

    Add data classes and test data

---
+++ lib/model/region_forecast.dart
@@ -0,0 +1,72 @@
+class RegionForecast {
+  final String regionNameThai;
+  final String regionNameEnglish;
+  final String descriptionThai;
+  final String descriptionEnglish;
+
+  const RegionForecast({
+    required this.regionNameThai,
+    required this.regionNameEnglish,
+    required this.descriptionThai,
+    required this.descriptionEnglish,
+  });
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      (other is RegionForecast &&
+          runtimeType == other.runtimeType &&
+          regionNameThai == other.regionNameThai &&
+          regionNameEnglish == other.regionNameEnglish &&
+          descriptionThai == other.descriptionThai &&
+          descriptionEnglish == other.descriptionEnglish);
+
+  @override
+  int get hashCode =>
+      regionNameThai.hashCode ^
+      regionNameEnglish.hashCode ^
+      descriptionThai.hashCode ^
+      descriptionEnglish.hashCode;
+
+  @override
+  String toString() {
+    return 'RegionForecast{' +
+        ' RegionNameThai: $regionNameThai,' +
+        ' RegionNameEnglish: $regionNameEnglish,' +
+        ' DescriptionThai: $descriptionThai,' +
+        ' DescriptionEnglish: $descriptionEnglish,' +
+        '}';
+  }
+
+  RegionForecast copyWith({
+    String? regionNameThai,
+    String? regionNameEnglish,
+    String? descriptionThai,
+    String? descriptionEnglish,
+  }) {
+    return RegionForecast(
+      regionNameThai: regionNameThai ?? this.regionNameThai,
+      regionNameEnglish: regionNameEnglish ?? this.regionNameEnglish,
+      descriptionThai: descriptionThai ?? this.descriptionThai,
+      descriptionEnglish: descriptionEnglish ?? this.descriptionEnglish,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'RegionNameThai': regionNameThai,
+      'RegionNameEnglish': regionNameEnglish,
+      'DescriptionThai': descriptionThai,
+      'DescriptionEnglish': descriptionEnglish,
+    };
+  }
+
+  factory RegionForecast.fromMap(Map<String, dynamic> map) {
+    return RegionForecast(
+      regionNameThai: map['RegionNameThai'] as String,
+      regionNameEnglish: map['RegionNameEnglish'] as String,
+      descriptionThai: map['DescriptionThai'] as String,
+      descriptionEnglish: map['DescriptionEnglish'] as String,
+    );
+  }
+}
