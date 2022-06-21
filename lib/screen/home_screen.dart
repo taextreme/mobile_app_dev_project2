33ae68f21e15674614dcb7a6eebbb6c7feec2a74
TaeXtream <59652919+taextreme@users.noreply.github.com>
2022-06-21 22:36:32 +0700

    Implement prototype home screen with weather Icon widget

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_app_dev_project2/component/base_card.dart';
+import 'package:mobile_app_dev_project2/model/daily_forecast.dart';
+import 'package:mobile_app_dev_project2/test_resources/test_forecast.dart';
+import 'package:mobile_app_dev_project2/utils/data_utils.dart';
+import 'package:mobile_app_dev_project2/utils/weather_utils.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -10,9 +15,21 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
+    DailyForecast forecast =
+        dailyForecastFromXML(dailyForecastTestData); // use test data
     return Scaffold(
         appBar: AppBar(
-      title: const Text('THAILAND DAILY FORECAST'),
-    ));
+          title: const Text('THAILAND DAILY FORECAST'),
+        ),
+        body: Column(
+          children: <Widget>[createLogoBaseCard(forecast)],
+        ));
+  }
+
+  BaseCard createLogoBaseCard(DailyForecast regionForecast) {
+    String description = regionForecast.overallDescriptionEnglish;
+    IconData icon = getWeatherIconFromData(description);
+    Color color = Colors.black54; // TODO: add logic for color selection
+    return BaseCard(theColor: color, theChild: Icon(icon, color: Colors.white));
   }
 }
