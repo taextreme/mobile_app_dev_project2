17f057103a8e9e35be0adebcfbfffbc3e6eb7912
TaeXtream <59652919+taextreme@users.noreply.github.com>
2022-06-21 22:34:14 +0700

    Add utils files

---
+++ lib/utils/data_utils.dart
@@ -0,0 +1,12 @@
+import 'dart:convert';
+
+import 'package:mobile_app_dev_project2/model/daily_forecast.dart';
+import 'package:xml2json/xml2json.dart';
+
+DailyForecast dailyForecastFromXML(String xml) {
+  final xmlJsonTransformer = Xml2Json();
+  xmlJsonTransformer.parse(xml);
+  String jsonData = xmlJsonTransformer.toBadgerfish();
+  Map<String, dynamic> mapData = json.decode(jsonData);
+  return DailyForecast.fromMap(mapData);
+}
---
+++ lib/utils/weather_utils.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:weather_icons/weather_icons.dart';
+
+IconData getWeatherIconFromData(String data) {
+  data = data.toLowerCase();
+  IconData iconData;
+  if (data.contains("rain")) {
+    iconData = WeatherIcons.rain;
+  } else if (data.contains("rain") && data.contains("wind")) {
+    iconData = WeatherIcons.rain_wind;
+  } else if (data.contains("storm")) {
+    iconData = WeatherIcons.storm_showers;
+  } else if (data.contains("thunder") && data.contains("storm")) {
+    iconData = WeatherIcons.thunderstorm;
+  } else if (data.contains("cloudy")) {
+    iconData = WeatherIcons.cloudy;
+  } else if (data.contains("cloudy") && data.contains("windy")) {
+    iconData = WeatherIcons.cloudy_windy;
+  } else if (data.contains("cloudy") && data.contains("gust")) {
+    iconData = WeatherIcons.cloudy_gusts;
+  } else if (data.contains("sunny")) {
+    iconData = WeatherIcons.day_sunny;
+  } else if (data.contains("sunny") && data.contains("overcast")) {
+    iconData = WeatherIcons.day_sunny_overcast;
+  } else if (data.contains("windy")) {
+    iconData = WeatherIcons.day_windy;
+  } else {
+    iconData = WeatherIcons.alien;
+  }
+  return iconData;
+}
