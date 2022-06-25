3079286f4b56c4f8b516de1ac541e4b47c4402f9
TaeXtream <59652919+taextreme@users.noreply.github.com>
2022-06-25 15:41:51 +0700

    Add Region widgets to home page

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -5,6 +5,8 @@ import 'package:mobile_app_dev_project2/test_resources/test_forecast.dart';
 import 'package:mobile_app_dev_project2/utils/data_utils.dart';
 import 'package:mobile_app_dev_project2/utils/widget_utils.dart';
 
+import '../model/region_forecast.dart';
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
@@ -22,20 +24,26 @@ class _HomeScreenState extends State<HomeScreen> {
           title: const Text('THAILAND DAILY FORECAST'),
         ),
         body: ListView(
-          children: <Widget>[
-            createLogoBaseCard(forecast),
-            BaseCard(
-                theColor: Colors.white,
-                theChild: Container(
-                    alignment: Alignment.topCenter,
-                    child: Text(forecast.date,
-                        style: const TextStyle(fontSize: 30)))),
-            BaseCard(
-                theColor: Colors.white,
-                theChild: Text(forecast.overallDescriptionEnglish,
-                    style: const TextStyle(fontSize: 18)))
-          ],
+          children: createHomePageBody(forecast),
         ));
+  }
+
+  List<Widget> createHomePageBody(DailyForecast forecast) {
+    List<Widget> body = <Widget>[
+      createLogoBaseCard(forecast),
+      Container(
+          color: Colors.white,
+          child: Container(
+              alignment: Alignment.topCenter,
+              child:
+                  Text(forecast.date, style: const TextStyle(fontSize: 30)))),
+      Container(
+          color: Colors.white,
+          child: Text(forecast.overallDescriptionEnglish,
+              style: const TextStyle(fontSize: 18))),
+    ];
+    body.addAll(getRegionBaseCards(forecast.regionsForecast));
+    return body;
   }
 
   BaseCard createLogoBaseCard(DailyForecast regionForecast) {
@@ -53,5 +61,23 @@ class _HomeScreenState extends State<HomeScreen> {
               color: Colors.white,
               size: 200.0,
             )));
+  }
+
+  List<BaseCard> getRegionBaseCards(List<RegionForecast> data) {
+    List<BaseCard> list = [];
+    List<Color> colors = [
+      Colors.red,
+      Colors.orange,
+      Colors.yellow,
+      Colors.green,
+      Colors.lightBlue,
+      Colors.blue,
+      Colors.purple
+    ];
+    for (var i = 0; i < data.length; i++) {
+      list.add(BaseCard(
+          theColor: colors[i], theChild: Text(data[i].regionNameEnglish)));
+    }
+    return list;
   }
 }
