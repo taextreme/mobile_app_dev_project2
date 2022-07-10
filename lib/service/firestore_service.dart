d123ba888b7cd00bf6f0c68f09a5c91943f9f757
Tan Kongpithaksilp <45220855+tangentino@users.noreply.github.com>
2022-07-11 00:13:47 +0700

    Added space

--- lib/service/firestore_service.dart
+++ lib/service/firestore_service.dart
@@ -3,6 +3,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:mobile_app_dev_project2/model/daily_forecast.dart';
 
 class FirestoreService {
+
   final forecastRef = FirebaseFirestore.instance.collection('forecasts').withConverter<DailyForecast>(
       fromFirestore: (snapshot, _) => DailyForecast.fromMap(snapshot.data()!),
       toFirestore: (dailyForecast, _) => dailyForecast.toMap()
